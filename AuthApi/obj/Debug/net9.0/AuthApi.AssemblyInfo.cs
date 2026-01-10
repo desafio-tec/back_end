9d66c4a56e2bb8bbcf043eeca8635e065642340f
LHDevData​ <lucas.mattenhauer@gmail.com>
2026-01-10 02:43:14 -0300

    novo dnet

--- AuthApi/obj/Debug/net9.0/AuthApi.AssemblyInfo.cs
+++ AuthApi/obj/Debug/net9.0/AuthApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22d23e0426db08eb960be8a3afc41c434d484e67")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8866a2a6aea89eb54b48bfb020ab5439b940e2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
